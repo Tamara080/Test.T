90ffe131aef17a3e924de70d01adea296a254c5b
Tamara080 <kurtsikidzetako@gmail.com>
2025-04-04 19:33:17 +0300

    homwork9

---
+++ Task2.js
@@ -0,0 +1,34 @@
+//Დაბეჭდეთ რიცხვები 2 დან 8 მდე
+/*for(let i=2;  i < 8; i++)
+{console.log(i);}*/
+//5 დან 35 - ის ჩათვლით დაბეჭდეთ ყოველი მეოთხე რიცხვი (უნდა დაიბეჭდოს 5,9,13…)
+/*for(let i=5; i<=35; i+=4)
+{console.log(i);}*/
+//დაბეჭდეთ 3 დან 8 მდე რიცხვების ნამრავლი
+/*let a=1;
+for(let i=3;i<8;i++)
+    a*=i;
+console.log(a);*/
+//Გამოაცხადეთ ობიექტი person რომელსაც აქვს firstName, lastName, age და დაბეჭდეთ person-სრული სახელი ( სახელი და გვარი)
+
+/*let person = {firstName:'nino',
+    lastName:'chachia',
+    age:30};*/
+    //console.log(person.firstName,person.lastName)
+   // დაბეჭდეთ მეოთხე დავალებაში გამოცხადებული პერსონის თითოეული property value 
+//console.log(person.firstName,person.lastName,person.age)
+/*for(const personkey in person){console.log(person[personkey]);
+*/
+// დაწერეთ მასივი fruits ელემენტებით ["Apple", "Banana", "Orange"]; დაბეჭდეთ მასივის თითოეული წევრი.
+/*let fruits=["apple","banana","orange"]
+ for (i=0;i<fruits.length;i++)
+    console.log(fruits[i]);*/
+ //დაამატეთ fruits მასივის თავში Grapes და ბოლოში  Pineapples დაბეჭდეთ fruits
+ /*fruits.unshift("grapes")
+ fruits.push("pineapples")
+ console.log(fruits)*/
+ //დაბეჭდეთ 1 დან 34 მდე რიცხვების ჯამი
+ /*let sum=0;
+ for(let i=1;i<=34;i++)
+    sum+=i;
+console.log(sum);*/
